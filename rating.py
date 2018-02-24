3190574aedb85e572cd7b6dce26d1e849b31d6a9
bLue <tbdblue@gmail.com>
2018-02-24 16:52:36 +0800

    Optimize code

--- rating.py
+++ rating.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import json
 import sys
 import math
@@ -10,10 +11,11 @@ class User(object):
         self.old_rating = int(old_rating)
         self.seed = 1.0
         self.handle = str(handle)
+        # official_new_rating: used for validating result
         self.official_new_rating = int(official_new_rating)
 
 
-class Calculator(object):
+class RatingCalculator(object):
 
     def __init__(self, users):
         self.user_list = []
@@ -43,25 +45,30 @@ class Calculator(object):
         return left
 
     def calculate(self):
+        # Calculate seed
         for i in range(len(self.user_list)):
             self.user_list[i].seed = 1.0
             for j in range(len(self.user_list)):
                 if i != j:
                     self.user_list[i].seed += self.cal_p(self.user_list[j], self.user_list[i])
+        # Calculate initial delta and sum_delta
         sum_delta = 0
         for user in self.user_list:
             user.delta = int(
                 (self.cal_rating(self.user_list, math.sqrt(user.rank * user.seed), user) - user.old_rating) / 2)
             sum_delta += user.delta
+        # Calculate first inc
         inc = int(-sum_delta / len(self.user_list)) - 1
         for user in self.user_list:
             user.delta += inc
         self.user_list = sorted(self.user_list, key=lambda x: x.old_rating, reverse=True)
+        # Calculate second inc
         s = min(len(self.user_list), int(4 * math.sqrt(len(self.user_list))))
         sum_s = 0
         for i in range(s):
             sum_s += self.user_list[i].delta
         inc = min(max(int(-sum_s / s), -10), 0)
+        # Calculate new rating
         for user in self.user_list:
             user.delta += inc
             user.new_rating = user.old_rating + user.delta
@@ -69,9 +76,14 @@ class Calculator(object):
 
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Usage: python3 rating.py [codeforces_contest_id]')
+        sys.exit(1)
     contest_id = sys.argv[1]
-    with open('cf_rating_' + contest_id + '.json', 'r') as f:
+    with open('cf_rating_{}.json'.format(contest_id), 'r') as f:
         test_users = json.loads(f.read())
+    # For consecutive users with same rank, we should set their ranks to the real rank of the last
+    # For example, initial standings are [1, 2, 2, 2, 5], and new standings will be [1, 4, 4, 4, 5]
     last_idx = 0
     last_rank = 1
     for i in range(1, len(test_users)):
@@ -81,9 +93,11 @@ if __name__ == '__main__':
             last_idx = i
             last_rank = test_users[i]['rank']
 
-    calculator = Calculator(test_users)
+    # Calculate rating changes
+    calculator = RatingCalculator(test_users)
     calculator.calculate()
     res = []
+    # validation: used for validating result
     validation = True
     for user in calculator.user_list:
         res.append({
@@ -95,6 +109,6 @@ if __name__ == '__main__':
         })
         if user.new_rating != user.official_new_rating:
             validation = False
-    with open('cf_rating_changes_' + contest_id + '.json', 'w') as f:
+    with open('cf_rating_changes_{}.json'.format(contest_id), 'w') as f:
         f.write(json.dumps(res, indent=4))
     print('Validation:', validation)
--- spider.py
+++ spider.py
@@ -4,8 +4,9 @@ import sys
 import requests
 
 
-def get_rating(contest_id=0):
-    url = 'http://codeforces.com/api/contest.ratingChanges?contestId=' + str(contest_id)
+# Get official rating changes data from Codeforces API
+def get_rating(contest_id):
+    url = 'http://codeforces.com/api/contest.ratingChanges?contestId={}'.format(contest_id)
     rst = requests.get(url)
     json_response = rst.content.decode()
     dict_json = json.loads(json_response)
@@ -15,15 +16,18 @@ def get_rating(contest_id=0):
         return None
 
 
-def work(contest_id=0):
+def work(contest_id):
     data = get_rating(contest_id)
     if data:
-        with open('cf_rating_{}.json'.format(contest_id), 'w') as fr:
-            fr.write(json.dumps(data, indent=4))
+        with open('cf_rating_{}.json'.format(contest_id), 'w') as f:
+            f.write(json.dumps(data, indent=4))
     else:
         print('error!')
 
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Usage: python3 spider.py [codeforces_contest_id]')
+        sys.exit(1)
     contest_id = sys.argv[1]
     work(contest_id)
